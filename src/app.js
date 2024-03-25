8501ce5fe629d9454c91260283c2492943e96655
RikrdoLeal <140850677+RikrdoLeal@users.noreply.github.com>
2024-03-25 21:08:10 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "my"];
+  let adjective = ["awesome", "big", "secret", "smart"];
+  let noun = ["page", "space", "blog", "post"];
+  let extension = [".net", ".com", ".es", ".ve"];
+  let domainName = [];
+  let ramdonDomain;
+
+  function getRandomName(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function generateDomainName() {
+    let arrays = [pronoun, adjective, noun, extension];
+    for (let i = 0; i < arrays.length; i++) {
+      domainName.push(getRandomName(arrays[i]));
+    }
+    ramdonDomain = domainName.join("");
+    return ramdonDomain;
+  }
+  console.log(generateDomainName());
+  document.getElementById("domain").innerHTML = ramdonDomain;
 };
